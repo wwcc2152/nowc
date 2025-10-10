e4d779d4500e2b7f0e3cc6734bb68488c60a7fa8
Cohee <18619528+Cohee1207@users.noreply.github.com>
2025-10-11 00:26:45 +0300

    NanoGPT: Add reasoning content display Fixes #4643

--- public/scripts/reasoning.js
+++ public/scripts/reasoning.js
@@ -125,6 +125,8 @@ export function extractReasoningFromData(data, {
                 case chat_completion_sources.POLLINATIONS:
                 case chat_completion_sources.MOONSHOT:
                 case chat_completion_sources.COMETAPI:
+                case chat_completion_sources.ELECTRONHUB:
+                case chat_completion_sources.NANOGPT:
                 case chat_completion_sources.CUSTOM: {
                     return data?.choices?.[0]?.message?.reasoning_content
                         ?? data?.choices?.[0]?.message?.reasoning
